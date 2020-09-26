2b5adc78e558131e3ce13ee89c19c9afc6ecdb19
wbriese <wbriese@gmail.com>
2020-09-26 15:39:57 +0200

    included Redux

--- react-tailwindcss/src/App.js
+++ react-tailwindcss/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 import "./style/main.css";
+import { createStore } from "redux";
+import { Provider} from "react-redux";
+import reducer from "./reducer.js";
+
 import MapComponent from './MapComponent/MapComponent';
 import ShipTable from './ShipTable/ShipTable.js';
 
+
+const store = createStore(reducer);
 
 function shipTable() {
   return (
@@ -11,18 +17,21 @@ function shipTable() {
     <div className="p-4 m-4 bg-blue-600">
         <h1 className="text-2xl font-bold text-white">Fleet Monitor</h1>
       </div>
-    <div className="h-64 flex flex-row">
+
+      <Provider store={store}>
+
+    <div className="flex flex-row">
       
-      <div className="p-4 m-4 bg-blue-600">
+      <div className="p-4 m-4 bg-blue-600 rounded">
         <ShipTable/>
       </div>
-      <button className="m-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">Button</button>
+      {/* <button className="m-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">Button</button> */}
 
-      
       <div className="m-4 rounded">
       <MapComponent/>
       </div>
     </div>
+    </Provider>
     </div>
   );
 }
