193ee975aa7bffc02c656405cf3872dbf4b68e8b
pintoedo <eduardoandrespo@gmail.com>
2020-10-02 18:18:38 +0200

    add infowindow to markers, fixes to be done

--- react-tailwindcss/src/components/MapComponent/MapComponent.tsx
+++ react-tailwindcss/src/components/MapComponent/MapComponent.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import {
+  GoogleMap,
+  InfoWindow,
+  LoadScript,
+  Marker,
+} from '@react-google-maps/api';
 import shipIcon from './icons8-cargo-ship-100.png';
 import { Ships } from '../../types/redux';
 import { Ship } from '../../types/ShipInterface';
@@ -26,6 +31,8 @@ type MapComponentProps = {
 };
 
 export const MapComponent: React.FC<MapComponentProps> = ({ rows, center }) => {
+  console.log(rows);
+
   let map;
   return (
     <div className="md:px-20 py-8 w-full">
@@ -49,7 +56,16 @@ export const MapComponent: React.FC<MapComponentProps> = ({ rows, center }) => {
                   map.panTo({ lat: ship.original.lat, lng: ship.original.lng });
                   map.setZoom(6);
                 }}
-              />
+              >
+                <InfoWindow key={ship.index}>
+                  <div>
+                    <p>Name: {ship.original.shipName}</p>
+                    <p>Owner: {ship.original.owner}</p>
+                    <p>Type: {ship.original.type}</p>
+                    <p>MMSI: {ship.original.mmsi}</p>
+                  </div>
+                </InfoWindow>
+              </Marker>
             ))}
           </GoogleMap>
         </LoadScript>
