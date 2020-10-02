2ccb6a15ac0a652978bfe69130fe62b51f4bb986
alexts404 <alexts404dev@gmail.com>
2020-10-02 16:42:16 +0200

    fix: move setRows into useEffect

--- react-tailwindcss/src/components/ShipTable/ShipTable.tsx
+++ react-tailwindcss/src/components/ShipTable/ShipTable.tsx
@@ -1,5 +1,5 @@
 import { useTable, useFilters, useGlobalFilter } from 'react-table';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { ShipsState } from '../../types/redux';
 import { DefaultColumnFilter } from '../DefaultColumnFilter/DefaultColumnFilter';
@@ -12,13 +12,9 @@ type ShipTableProps = {
 const EmptyDiv: React.FC<unknown> = () => <div></div>;
 
 function ShipTable({ setRows }: ShipTableProps) {
-  const shipData = useSelector(function (state: ShipsState) {
-    return state.ships;
-  });
+  const shipData = useSelector((state: ShipsState) => state.ships);
 
   const data = React.useMemo(() => shipData, [shipData]);
-
-  console.log('data', data);
 
   const columns = React.useMemo(
     () => [
@@ -33,58 +29,68 @@ function ShipTable({ setRows }: ShipTableProps) {
       {
         Header: 'Ship Type',
         accessor: 'type',
-        Filter: SelectColumnFilter,
-        filter: 'includes',
+        // Filter: SelectColumnFilter,
+        // filter: 'includes',
       },
       {
         Header: 'Owner',
         accessor: 'owner',
-        Filter: SelectColumnFilter,
-        filter: 'includes',
+        // Filter: SelectColumnFilter,
+        // filter: 'includes',
       },
       {
         Header: 'Latitude',
         accessor: 'lat',
-        Filter: <EmptyDiv />,
-        filter: 'includes',
+        // Filter: <EmptyDiv />,
+        // filter: 'includes',
       },
       {
         Header: 'Longitude',
         accessor: 'lng',
-        Filter: <EmptyDiv />,
-        filter: 'includes',
+        // Filter: <EmptyDiv />,
+        // filter: 'includes',
       },
       {
         Header: 'updated',
         accessor: 'updated',
-        Filter: <EmptyDiv />,
-        filter: 'includes',
+        // Filter: <EmptyDiv />,
+        // filter: 'includes',
       },
     ],
     [],
   );
 
-  const defaultColumn = React.useMemo(
-    () => ({
-      Filter: DefaultColumnFilter,
-    }),
-    [],
-  );
+  // const defaultColumn = React.useMemo(
+  //   () => ({
+  //     Filter: DefaultColumnFilter,
+  //   }),
+  //   [],
+  // );
 
-  const filterTypes = React.useMemo(
-    () => ({
-      text: (rows, id, filterValue) => {
-        return rows.filter((row) => {
-          const rowValue = row.values[id];
-          return rowValue !== undefined
-            ? String(rowValue)
-                .toLowerCase()
-                .startsWith(String(filterValue).toLowerCase())
-            : true;
-        });
-      },
-    }),
-    [],
+  // const filterTypes = React.useMemo(
+  //   () => ({
+  //     text: (rows, id, filterValue) => {
+  //       return rows.filter((row) => {
+  //         const rowValue = row.values[id];
+  //         return rowValue !== undefined
+  //           ? String(rowValue)
+  //               .toLowerCase()
+  //               .startsWith(String(filterValue).toLowerCase())
+  //           : true;
+  //       });
+  //     },
+  //   }),
+  //   [],
+  // );
+  const tableInstance = useTable(
+    {
+      columns,
+      data,
+      // defaultColumn,
+      // filterTypes,
+    },
+    useFilters,
+    useGlobalFilter,
   );
 
   const {
@@ -93,67 +99,98 @@ function ShipTable({ setRows }: ShipTableProps) {
     headerGroups,
     rows,
     prepareRow,
-  } = useTable(
-    {
-      columns,
-      data,
-      defaultColumn,
-      filterTypes,
-    },
-    useFilters,
-    useGlobalFilter,
-  );
+  } = tableInstance;
 
-  setRows(rows);
+  useEffect(() => {
+    setRows(rows);
+  }, [rows]);
 
   return (
     // TODO: add reasonable keys
 
-    <div className="md: px-20 py-8 w-full">
-      <div className="shadow overflow-hidden rounded border-b border-gray-200">
-        <table className="min-w-full bg-white" {...getTableProps()}>
-          <thead className="bg-gray-800 text-white">
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()} key="KEY">
-                {headerGroup.headers.map((column) => (
-                  <th
-                    className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm"
-                    {...column.getHeaderProps()}
-                    key="ANOTHERKEY"
-                  >
-                    {column.render('Header')}
-                    <div className="text-black">
-                      {column.canFilter ? column.render('Filter') : null}
-                    </div>
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
+    // <div className="md: px-20 py-8 w-full">
+    //   <div className="shadow overflow-hidden rounded border-b border-gray-200">
+    //     <table className="min-w-full bg-white" {...getTableProps()}>
+    //       <thead className="bg-gray-800 text-white">
+    //         {headerGroups.map((headerGroup) => (
+    //           <tr {...headerGroup.getHeaderGroupProps()} key="KEY">
+    //             {headerGroup.headers.map((column) => (
+    //               <th
+    //                 className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm"
+    //                 {...column.getHeaderProps()}
+    //                 key="ANOTHERKEY"
+    //               >
+    //                 {column.render('Header')}
+    //                 <div className="text-black">
+    //                   {column.canFilter ? column.render('Filter') : null}
+    //                 </div>
+    //               </th>
+    //             ))}
+    //           </tr>
+    //         ))}
+    //       </thead>
 
-          <tbody className="text-gray-700" {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
+    //       <tbody className="text-gray-700" {...getTableBodyProps()}>
+    //         {rows.map((row) => {
+    //           prepareRow(row);
+    //           return (
+    //             <tr {...row.getRowProps()} key="YET A THIRD KEY">
+    //               {row.cells.map((cell) => {
+    //                 return (
+    //                   <td
+    //                     className="w-1/3 text-left py-3 px-4"
+    //                     {...cell.getCellProps()}
+    //                     key="AND A FOURTH"
+    //                   >
+    //                     {cell.render('Cell')}
+    //                   </td>
+    //                 );
+    //               })}
+    //             </tr>
+    //           );
+    //         })}
+    //       </tbody>
+    //     </table>
+    //   </div>
+    // </div>
+
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()} key="headers">
+            {headerGroup.headers.map((column) => {
               return (
-                <tr {...row.getRowProps()} key="YET A THIRD KEY">
-                  {row.cells.map((cell) => {
-                    return (
-                      <td
-                        className="w-1/3 text-left py-3 px-4"
-                        {...cell.getCellProps()}
-                        key="AND A FOURTH"
-                      >
-                        {cell.render('Cell')}
-                      </td>
-                    );
-                  })}
-                </tr>
+                <th
+                  {...column.getHeaderProps()}
+                  key={'header-' + column.Header}
+                >
+                  {column.render('Header')}
+                </th>
               );
             })}
-          </tbody>
-        </table>
-      </div>
-    </div>
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()} key={row.id}>
+              {row.cells.map((cell) => {
+                return (
+                  <td
+                    {...cell.getCellProps()}
+                    key={'row' + cell.row.id + '-' + cell.column.Header}
+                  >
+                    {cell.render('Cell')}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
 
