809f86532135302e8f94d7aecb05b3b5c6245125
alexts404 <alexts404dev@gmail.com>
2020-10-05 11:00:17 +0200

    feat: add types for apiCalls

---
+++ react-tailwindcss/src/types/apiDefs.ts
@@ -0,0 +1,75 @@
+export interface Positions {
+  [time: string]: {
+    time: Date;
+    lat: number;
+    lng: number;
+  };
+}
+
+interface LatLng {
+  lat: number;
+  lng: number;
+}
+
+export class VoyageInfo {
+  mmsi: number;
+  destination: string;
+  lastPortId: string;
+  lastPortName: string;
+  lastPortUnlocode: string;
+  lastPortTime: Date;
+  nexPortId: string;
+  nextPortName: string;
+  nextPortUnlocode: string;
+  eta: Date;
+  etaCalc: Date;
+  distanceTravelled: number;
+  distanceToGo: number;
+  speedCalc: number;
+  draught: number;
+  draughtMax: number;
+  loadStatusName: string;
+  route: LatLng[];
+
+  constructor([
+    mmsi,
+    destination,
+    lastPortId,
+    lastPortName,
+    lastPortUnlocode,
+    lastPortTime,
+    nexPortId,
+    nextPortName,
+    nextPortUnlocode,
+    eta,
+    etaCalc,
+    distanceTravelled,
+    distanceToGo,
+    speedCalc,
+    draught,
+    draughtMax,
+    loadStatusName,
+    route,
+  ]: string[]) {
+    this.mmsi = parseInt(mmsi);
+    this.destination = destination;
+    this.lastPortId = lastPortId;
+    this.lastPortName = lastPortName;
+    this.lastPortUnlocode = lastPortUnlocode;
+    this.lastPortTime = new Date(lastPortTime);
+    this.nexPortId = nexPortId;
+    this.nextPortName = nextPortName;
+    this.nextPortUnlocode = nextPortUnlocode;
+    this.eta = new Date(eta);
+    this.etaCalc = new Date(etaCalc);
+    this.distanceTravelled = parseInt(distanceTravelled);
+    this.distanceToGo = parseInt(distanceToGo);
+    this.speedCalc = parseInt(speedCalc);
+    this.draught = parseInt(draught);
+    this.draughtMax = parseInt(draughtMax);
+    this.loadStatusName = loadStatusName;
+    this.route = this.parse(route);
+  }
+
+  parse(route: string): LatLng[] {}
+}
