ea286c52b97ac3a28363074a5cd9776f68113b96
pintoedo <eduardoandrespo@gmail.com>
2020-10-06 20:04:36 +0200

    added multiple changes / ports data / ships model / port models

---
+++ react-tailwindcss/backEnd/src/models/ports.ts
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+
+interface IPort {
+  name: string;
+  city: string;
+  contry: string;
+  alias: [];
+  regions: [];
+  coordinates: number[];
+  province: string;
+  timezone: string;
+  unlocs: string[];
+  code: number;
+}
+
+interface PortModelInterface extends mongoose.Model<PortDoc> {
+  build(attr: IPort): PortDoc;
+}
+
+interface PortDoc extends mongoose.Document {
+  name: string;
+  city: string;
+  contry: string;
+  alias: [];
+  regions: [];
+  coordinates: number[];
+  province: string;
+  timezone: string;
+  unlocs: string[];
+  code: number;
+}
+
+const portSchema = new mongoose.Schema({
+  name: String,
+  city: String,
+  contry: String,
+  alias: Array,
+  regions: Array,
+  coordinates: Array,
+  province: String,
+  timezone: String,
+  unlocs: Array,
+  code: Number,
+});
+
+portSchema.statics.build = (attr: IPort) => {
+  return new Port(attr);
+};
+const Port = mongoose.model<PortDoc, PortModelInterface>('Port', portSchema);
+
+export { Port };
