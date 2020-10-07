09b5cb6145307a1c9a7989b96e188db111f63199
alexts404 <alexts404dev@gmail.com>
2020-10-07 20:17:47 +0200

    feat: implement time slider

--- react-tailwindcss/src/components/Map/MapComponent/MapComponent.tsx
+++ react-tailwindcss/src/components/Map/MapComponent/MapComponent.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import { Ship } from '../../../types/ShipInterface';
+import { PointInHistory, Ship } from '../../../types/ShipInterface';
 import { Row } from 'react-table';
 import { ShipMapLayer } from '../ShipMapLayer/ShipMapLayer';
+import { TimeSlider } from '../TimeSlider/TimeSlider';
 
 const containerStyle = {
   width: '100%',
@@ -19,11 +20,113 @@ interface MapComponentProps {
   center: Center;
 }
 
+const history = {
+  previousStates: [
+    {
+      time: Date.now() - 300000000,
+      ships: [
+        {
+          shipName: 'Zea Fame',
+          mmsi: '255806176',
+          type: '12000t 2x350t 20kn',
+          owner: 'UHL',
+          lat: 52.44043,
+          lng: 13.371147,
+          updated: 'mock',
+        },
+        {
+          shipName: 'Anne_Sofie',
+          mmsi: '218412000',
+          type: '12000t 2x350t 20kn',
+          owner: 'SAL',
+          lat: 51.44043,
+          lng: 13.371147,
+          updated: 'mock',
+        },
+      ],
+    },
+    {
+      time: Date.now() - 200000000,
+      ships: [
+        {
+          shipName: 'Zea Fame',
+          mmsi: '255806176',
+          type: '12000t 2x350t 20kn',
+          owner: 'UHL',
+          lat: 52.44043,
+          lng: 13.371147,
+          updated: 'mock',
+        },
+        {
+          shipName: 'Anne_Sofie',
+          mmsi: '218412000',
+          type: '12000t 2x350t 20kn',
+          owner: 'SAL',
+          lat: 51.44043,
+          lng: 13.371147,
+          updated: 'mock',
+        },
+      ],
+    },
+    {
+      time: Date.now() - 20000000,
+      ships: [
+        {
+          shipName: 'Zea Fame',
+          mmsi: '255806176',
+          type: '12000t 2x350t 20kn',
+          owner: 'UHL',
+          lat: 53.44043,
+          lng: 14.371147,
+          updated: 'mock',
+        },
+        {
+          shipName: 'Anne_Sofie',
+          mmsi: '218412000',
+          type: '12000t 2x350t 20kn',
+          owner: 'SAL',
+          lat: 51.44043,
+          lng: 10.371147,
+          updated: 'mock',
+        },
+      ],
+    },
+  ],
+  futureStates: [
+    {
+      time: Date.now() + 1000000,
+      ships: [
+        {
+          shipName: 'Zea Fame',
+          mmsi: '255806176',
+          type: '12000t 2x350t 20kn',
+          owner: 'UHL',
+          lat: 50.44043,
+          lng: 12.371147,
+          updated: 'mock',
+        },
+        {
+          shipName: 'Anne_Sofie',
+          mmsi: '218412000',
+          type: '12000t 2x350t 20kn',
+          owner: 'SAL',
+          lat: 42.44043,
+          lng: 13.371147,
+          updated: 'mock',
+        },
+      ],
+    },
+  ],
+};
+
 export const MapComponent: React.FC<MapComponentProps> = ({
   rows,
   center,
 }: MapComponentProps) => {
   const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [pointInHistory, setPointInHistory] = useState<PointInHistory>(
+    history.previousStates[history.previousStates.length - 1],
+  );
 
   const loadHandler = (loadedMap: google.maps.Map) => {
     setMap(loadedMap);
@@ -34,7 +137,7 @@ export const MapComponent: React.FC<MapComponentProps> = ({
   };
 
   return (
-    <div className="md:px-20 py-8 w-full">
+    <div className="p-20 py-8 w-full">
       <div className="shadow overflow-hidden rounded border-b border-gray-200">
         <LoadScript googleMapsApiKey={process.env.REACT_APP_API_MAPS}>
           <GoogleMap
@@ -44,10 +147,11 @@ export const MapComponent: React.FC<MapComponentProps> = ({
             onLoad={loadHandler}
             onUnmount={unmountHandler}
           >
-            <ShipMapLayer map={map} ships={rows} />
+            <ShipMapLayer map={map} ships={pointInHistory.ships} />
           </GoogleMap>
         </LoadScript>
       </div>
+      <TimeSlider setPointInHistory={setPointInHistory} history={history} />
     </div>
   );
 };
