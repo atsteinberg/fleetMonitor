20803b19db7fa30ed8ae0609853558f6cb6e3e5c
alexts404 <alexts404dev@gmail.com>
2020-10-08 07:25:18 +0200

    feat: add cors

--- react-tailwindcss/backEnd/src/index.ts
+++ react-tailwindcss/backEnd/src/index.ts
@@ -1,24 +1,25 @@
 import express from 'express';
 import { json } from 'body-parser';
 import { routerSpy } from './routes/routes';
+import cors from 'cors';
 import mongoose from 'mongoose';
+
+const PORT = process.env.DB_PORT || 3001;
 
 const app = express();
 app.use(json());
+app.use(
+  cors({
+    origin: process.env.APP_PORT || 'http://localhost:3000',
+  }),
+);
 app.use(routerSpy);
 
-mongoose.connect(
-  'mongodb://localhost:27017/fleetpsy',
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log('Connected to FleetSPY DB');
-  },
-);
-
-app.listen(3090, () => {
-  console.log('Server running on port 3090');
+const db = mongoose.connection;
+db.on('error', (err) => console.error(err));
+db.once('open', () => {
+  console.log('🔌 Successfully connected to db');
+  app.listen(PORT, () => {
+    console.log(`🚀 Server listening at http://localhost:${PORT}`);
+  });
 });
